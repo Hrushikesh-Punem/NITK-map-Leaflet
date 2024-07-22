f4b0c2f4470c7547d19b4d0f8e6a09c0605d1945
Hrushikesh Punem <134188782+Hrushikesh-Punem@users.noreply.github.com>
2024-07-22 17:05:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const myMap = L.map('map').setView([13.0102, 74.7938], 16.5);
 
 //Using CDN's
 //const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
 //Using local files
 //const tileUrl = './map.geojson';
+
 //Using server
 //const tileUrl = 'http://10.100.15.184:8000/NITK_map.geojson';
-//const tileUrl = './map_geojson';
 const tileUrl = 'http://10.100.15.184:8080/tile/{z}/{x}/{y}.png';
+
 
 
 const tileLayer = L.tileLayer(tileUrl,{maxzoom:19,});
@@ -19,9 +21,6 @@ tileLayer.addTo(myMap);
 // .then(data => {
 //     L.geoJSON(data).addTo(myMap);
 // });
-
-
-
 
 
 
